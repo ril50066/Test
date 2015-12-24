5a2cef5e8747176dbdcb6f576ee0fdf880b52be6
Fetics <ril50066@gmail.com>
2015-12-24 16:10:24 +0800

    newV
    newV

---
+++ TestPython/TestList.py
@@ -0,0 +1,4 @@
+mylist = [0 , "hello world" , 123 , "TEST"];
+
+for ele in mylist:
+    print(ele)
---
+++ TestPython/testImportModule.py
@@ -0,0 +1,6 @@
+from testModule import print_lol
+
+mylist = [0 , "hello world" , 123 , "TEST" , ["ele1" , "ele2" , "ele4"]];
+
+print_lol(mylist)
+print_lol(mylist)
